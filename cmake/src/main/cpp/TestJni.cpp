c11013ae67497cc70269bda480f13cb6fb55c7e0
李锦涛 <tao351992257@163.com>
2022-09-21 00:02:42 +0800

    Commit 定义结构体

--- cmake/src/main/cpp/TestJni.cpp
+++ cmake/src/main/cpp/TestJni.cpp
@@ -13,30 +13,34 @@
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG,  __VA_ARGS__);
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG,  __VA_ARGS__);
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG,  __VA_ARGS__);
+
+struct PublicAttr {
+    jclass user;
+    jobject userObj;
+} pubAttr;
+
 extern "C"
 JNIEXPORT jobject JNICALL
 Java_com_example_cmake_api_TestApi_test(JNIEnv *env, jobject thiz) {
-    jclass jUser;
-    jobject jUserObj;
     jmethodID jSetUsernameMethodId;
     jmethodID jSetPasswordMethodId;
     jmethodID jGetUsernameMethodId;
     jmethodID jGetPasswordMethodId;
     jstring jUserName;
     jstring jPassword;
-    jUser = env->FindClass("com/example/cmake/bean/User");
-    jUserObj = env->AllocObject(jUser);
-    jSetUsernameMethodId = env->GetMethodID(jUser,"setUsername", "(Ljava/lang/String;)V");
-    jSetPasswordMethodId = env->GetMethodID(jUser,"setPassword","(Ljava/lang/String;)V");
+    pubAttr.user = env->FindClass("com/example/cmake/bean/User");
+    pubAttr.userObj = env->AllocObject(pubAttr.user);
+    jSetUsernameMethodId = env->GetMethodID(pubAttr.user,"setUsername", "(Ljava/lang/String;)V");
+    jSetPasswordMethodId = env->GetMethodID(pubAttr.user,"setPassword","(Ljava/lang/String;)V");
     const char* userName = "李乐祺";
     const char* password = "123456";
-    env->CallVoidMethod(jUserObj,jSetUsernameMethodId,env->NewStringUTF(userName));
-    env->CallVoidMethod(jUserObj,jSetPasswordMethodId,env->NewStringUTF(password));
-    jGetUsernameMethodId = env->GetMethodID(jUser,"getUsername", "()Ljava/lang/String;");
-    jGetPasswordMethodId = env->GetMethodID(jUser,"getPassword","()Ljava/lang/String;");
-    jUserName = (jstring)env->CallObjectMethod(jUserObj,jGetUsernameMethodId);
-    jPassword = (jstring)env->CallObjectMethod(jUserObj,jGetPasswordMethodId);
-    LOGD("username:%s",jUserName)
-    LOGD("password:%s",jPassword)
-    return jUserObj;
+    env->CallVoidMethod(pubAttr.userObj,jSetUsernameMethodId,env->NewStringUTF(userName));
+    env->CallVoidMethod(pubAttr.userObj,jSetPasswordMethodId,env->NewStringUTF(password));
+    jGetUsernameMethodId = env->GetMethodID(pubAttr.user,"getUsername", "()Ljava/lang/String;");
+    jGetPasswordMethodId = env->GetMethodID(pubAttr.user,"getPassword","()Ljava/lang/String;");
+    jUserName = (jstring)env->CallObjectMethod(pubAttr.userObj,jGetUsernameMethodId);
+    jPassword = (jstring)env->CallObjectMethod(pubAttr.userObj,jGetPasswordMethodId);
+    LOGD("username:%s",env->GetStringUTFChars(jUserName,NULL))
+    LOGD("password:%s",env->GetStringUTFChars(jPassword,NULL))
+    return pubAttr.userObj;
 }
